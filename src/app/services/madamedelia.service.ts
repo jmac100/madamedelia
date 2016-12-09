557ecef9a82083746da0505593e80bf6a1b434b0
Jim MacDonald <Jim MacDonald>
2016-12-09 12:42:50 -0500

    upgrade angular-cli to 1.0.0-beta.22-1

--- src/app/services/madamedelia.service.ts
+++ src/app/services/madamedelia.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
 @Injectable()
